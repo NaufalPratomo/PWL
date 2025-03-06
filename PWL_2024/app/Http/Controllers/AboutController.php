c7abd478be49c6a1decef07728e30c31330ef6f6
NaufalPratomo <naufalpratomo8@gmail.com>
2025-03-06 23:07:51 +0700

    add: Praktikum 1, 2, & 3

---
+++ PWL_2024/app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function __invoke()
+    {
+        return 'NIM: 234172075, Nama: Muhammad Naufal Pratomo';
+    }
+}
--- PWL_2024/routes/web.php
+++ PWL_2024/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,12 +26,28 @@ Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/welcome', [PageController::class, 'index']);
-Route::get('/about', [PageController::class, 'about']);
+Route::get('/welcome', HomeController::class);
+Route::get('/about', AboutController::class);
 Route::get('/user/{name?}', function ($name = 'John') {
     return 'Nama saya ' . $name;
 });
 Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 });
-Route::get('/articles/{id}', [PageController::class, 'articles']);
+Route::get('/articles/{id}', ArticleController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index',
+    'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+    'create',
+    'store',
+    'update',
+    'destroy'
+]);
+
+Route::get('/greeting', [
+    WelcomeController::class,
+    'greeting'
+]);
