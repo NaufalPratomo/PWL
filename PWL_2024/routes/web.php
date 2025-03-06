9cdf799b6b0225793926e76bcbb6845ca82fa885
NaufalPratomo <naufalpratomo8@gmail.com>
2025-03-06 21:44:03 +0700

    first commit

---
+++ PWL_2024/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+Route::get('/world', function () {
+    return 'World';
+});
+Route::get('/welcome', function () {
+    return 'Selamat Datang';
+});
+Route::get('/about', function () {
+    return 'NIM: 234172075, Nama: Muhammad Naufal Pratomo';
+});
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
