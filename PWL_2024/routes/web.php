345a571c5180bcc07e5cba7923d900cc2d73a583
NaufalPratomo <naufalpratomo8@gmail.com>
2025-03-06 22:23:39 +0700

    feat: memindahkan logika eksekusi ke dalam controller dengan nama PageController.

--- PWL_2024/routes/web.php
+++ PWL_2024/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,24 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/hello', function () {
     return 'Hello World';
 });
+
 Route::get('/world', function () {
     return 'World';
 });
-Route::get('/welcome', function () {
-    return 'Selamat Datang';
-});
-Route::get('/about', function () {
-    return 'NIM: 234172075, Nama: Muhammad Naufal Pratomo';
-});
-Route::get('/user/{name}', function ($name) {
+
+Route::get('/welcome', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/user/{name?}', function ($name = 'John') {
     return 'Nama saya ' . $name;
 });
 Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 });
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID ' . $id;
-});
-Route::get('/user/{name?}', function ($name = 'John') {
-    return 'Nama saya ' . $name;
-});
+Route::get('/articles/{id}', [PageController::class, 'articles']);
