ab42d732cb76d7bdcee6c039fc36f0ab93e2a31f
NaufalPratomo <naufalpratomo8@gmail.com>
2025-03-15 08:59:54 +0700

    ADD: JS 4 (Praktikum 2.4)

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -12,11 +12,18 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $user = UserModel::where('level_id', 2)->count();
-        // dd($user);
-        // return view('user', ['data' => $user]);
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager 33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
+        return view('user', ['data' => $user]);
 
-        $userCount = UserModel::where('level_id', 2)->count();
-        return view('user', ['userCount' => $userCount]);
+        // $userCount = UserModel::where('level_id', 2)->count();
+        // return view('user', ['userCount' => $userCount]);
     }
 }
--- POS/resources/views/user.blade.php
+++ POS/resources/views/user.blade.php
@@ -8,7 +8,7 @@
 <body>
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
-        {{-- <tr>
+        <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
@@ -19,14 +19,14 @@
                 <td>{{ $data->username }}</td>
                 <td>{{ $data->nama }}</td>
                 <td>{{ $data->level_id }}</td>
-            </tr> --}}
+            </tr>
 
-            <tr>
+            {{-- <tr>
                 <th>Jumlah Pengguna</th>
             </tr>
             <tr>
                 <td>{{ $userCount }}</td>
-            </tr>
+            </tr> --}}
     </table>
 </body>
 
