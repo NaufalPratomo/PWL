92683451a0ea24f7aeff842cbe955105e46404f3
NaufalPratomo <naufalpratomo8@gmail.com>
2025-05-11 20:49:47 +0700

    add: JS 7 (Praktikum 2)

--- POS/app/Http/Middleware/AuthorizeUser.php
+++ POS/app/Http/Middleware/AuthorizeUser.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
@@ -8,13 +7,24 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AuthorizeUser
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
-        return $next($request);
-    }
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @param \Closure $next
+	 * @param string $role
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	public function handle(Request $request, Closure $next, $role): Response
+	{
+		$user = $request->user(); // Retrieve the logged-in user
+
+		// Check if the user has the required role
+		if ($user->hasRole($role)) {
+			return $next($request);
+		}
+
+		// If the user does not have the role, return a 403 error
+		abort(403, 'Forbidden. Kamu tidak punya akses ke halaman ini.');
+	}
 }
--- POS/routes/web.php
+++ POS/routes/web.php
@@ -20,12 +20,15 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::pattern('id', '[0-9]+'); // artinya ketika ada parameter (id), maka harus berupa angka
+Route::pattern('id', '[0-9]+'); // artinya ketika ada parameter {id}, maka harus berupa angka
+
 Route::get('login', [AuthController::class, 'login'])->name('login');
-Route::post('login', [AuthController::class, 'postlogin']);
+Route::post('login', [AuthController::class, 'postLogin']);
 Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
+
 Route::middleware(['auth'])->group(function () { // artinya semua route di dalam group ini harus login dulu
 
+    // masukkan semua route yang perlu autentikasi di sini
     Route::get('/', [WelcomeController::class, 'index']);
 
     Route::group(['prefix' => 'user'], function () {
@@ -117,4 +120,22 @@ Route::middleware(['auth'])->group(function () { // artinya semua route di dalam
         Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']); // Untuk hapus data barang Ajax
         Route::delete('/{id}', [BarangController::class, 'destroy']);     // Menghapus data barang
     });
+});
+
+Route::middleware(['auth'])->group(function () { // artinya semua route di dalam group ini harus login dulu
+    Route::get('/', [WelcomeController::class, 'index']);
+
+    // route Level
+    // artinya semua route di dalam group ini harus punya role ADM (Administrator)
+    Route::middleware(['authorize:ADM'])->group(function () {
+        Route::get('/level', [LevelController::class, 'index']);
+        Route::post('/level/list', [LevelController::class, 'list']); // untuk list json datatables
+        Route::get('/level/create', [LevelController::class, 'create']);
+        Route::post('/level', [LevelController::class, 'store']);
+        Route::get('/level/{id}/edit', [LevelController::class, 'edit']); // untuk tampilkan form edit
+        Route::put('/level/{id}', [LevelController::class, 'update']); // untuk proses update data
+        Route::delete('/level/{id}', [LevelController::class, 'destroy']); // untuk proses hapus data
+    });
+
+    // route Kategori
 });
