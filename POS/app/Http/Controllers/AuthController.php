1ffd1ad76dd7f241eba44eeaf11d8791976c3a0a
NaufalPratomo <naufalpratomo8@gmail.com>
2025-05-11 22:00:01 +0700

    add: JS 7 (Tugas 4)

--- POS/app/Http/Controllers/AuthController.php
+++ POS/app/Http/Controllers/AuthController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\UserModel;
+use App\Models\LevelModel;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -33,6 +36,47 @@ class AuthController extends Controller
         }
         return redirect('login');
     }
+
+    public function register()
+    {
+        $level = LevelModel::select('level_id', 'level_kode', 'level_nama')->get();
+
+        return view('auth.register', compact('level'));
+    }
+
+    public function postRegister(Request $request)
+    {
+        $rules = [
+            'level_id' => 'required|integer',
+            'username' => 'required|string|min:3|unique:m_user,username',
+            'nama'     => 'required|string|max:100',
+            'password' => 'required|min:5|confirmed',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validasi Gagal',
+                'msgField' => $validator->errors(),
+            ]);
+        }
+
+        $user = new UserModel();
+        $user->level_id = $request->level_id;
+        $user->username = $request->username;
+        $user->password = bcrypt($request->password);
+        $user->nama = $request->nama;
+        $user->save();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Registrasi Berhasil. Silakan login.',
+            'redirect' => url('login')
+        ]);
+    }
+
     public function logout(Request $request)
     {
         Auth::logout();
--- POS/resources/views/auth/register.blade.php
+++ POS/resources/views/auth/register.blade.php
@@ -4,40 +4,58 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login Pengguna</title><!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallb
-ack">
+    <title>Register Pengguna</title>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="{{ asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css')
-}}">
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
     <!-- SweetAlert2 -->
-
-    <link rel="stylesheet" href="{{ asset('adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-
-4.min.css') }}">
-
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
 </head>
 
-<body class="hold-transition login-page">
-    <div class="login-box">
-        <!-- /.login-logo -->
+<body class="hold-transition register-page">
+    <div class="register-box">
         <div class="card card-outline card-primary">
-            <div class="card-header text-center"><a href="{{ url('/') }}" class="h1"><b>Admin</b>LTE</a></div>
+            <div class="card-header text-center">
+                <a href="{{ url('/') }}" class="h1"><b>Admin</b>LTE</a>
+            </div>
             <div class="card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
-                <form action="{{ url('login') }}" method="POST" id="form-login">
+                <p class="login-box-msg">Register a new user</p>
+                <form action="{{ url('register') }}" method="POST" id="form-register">
                     @csrf
                     <div class="input-group mb-3">
+                        <select name="level_id" id="level_id" class="form-control" required>
+                            <option value="">- Pilih Level -</option>
+                            @foreach ($level as $l)
+                                @if ($l->level_kode != 'ADM')
+                                    <option value="{{ $l->level_id }}">{{ $l->level_nama }}</option>
+                                @endif
+                            @endforeach
+                        </select>
+                        <small id="error-level_id" class="error-text text-danger"></small>
+                    </div>
+                    <div class="input-group">
                         <input type="text" id="username" name="username" class="form-control" placeholder="Username">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
                             </div>
                         </div>
-                        <small id="error-username" class="error-text text-danger"></small>
+                    </div>
+                    <small id="error-username" class="error-text text-danger"></small>
+                    <div class="input-group mb-3 mt-3">
+                        <input type="text" id="nama" name="nama" class="form-control" placeholder="Nama">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                        <small id="error-nama" class="error-text text-danger"></small>
                     </div>
                     <div class="input-group mb-3">
                         <input type="password" id="password" name="password" class="form-control"
@@ -49,31 +67,31 @@ ack">
                         </div>
                         <small id="error-password" class="error-text text-danger"></small>
                     </div>
-                    <div class="col-12 text-center my-3">
-                        <p class="mb-0">
-                            <span>Don't have an account? </span><a href="{{ url('register') }}"
-                                class="text-center">Register</a>
-                        </p>
-                    </div>
-                    <div class="row my-3">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember"><label for="remember">Remember Me</label>
+                    <div class="input-group mb-0">
+                        <input type="password" id="password_confirmation" name="password_confirmation"
+                            class="form-control" placeholder="Confirm password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
                             </div>
                         </div>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                        <small id="error-password_confirmation" class="error-text text-danger"></small>
+                    </div>
+                    <div class="row my-3">
+                        <div class="col-12 text-center">
+                            <p>
+                                <span>Have an account? </span><a href="{{ url('login') }}" class="text-center">Login</a>
+                            </p>
                         </div>
-                        <!-- /.col -->
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary btn-block">Register</button>
+                        </div>
                     </div>
                 </form>
             </div>
-            <!-- /.card-body -->
         </div>
-        <!-- /.card -->
     </div>
-    <!-- /.login-box -->
+
     <!-- jQuery -->
     <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
     <!-- Bootstrap 4 -->
@@ -85,25 +103,46 @@ ack">
     <script src="{{ asset('adminlte/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
     <!-- AdminLTE App -->
     <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
+
     <script>
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
         $(document).ready(function () {
-            $("#form-login").validate({
+            $("#form-register").validate({
                 rules: {
-                    username: { required: true, minlength: 3, maxlength: 20 },
-                    password: { required: true, minlength: 5, maxlength: 20 }
+                    username: {
+                        required: true,
+                        minlength: 3,
+                        maxlength: 20
+                    },
+                    nama: {
+                        required: true,
+                        minlength: 3,
+                        maxlength: 100
+                    },
+                    level_id: {
+                        required: true,
+                        number: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    password: {
+                        required: true,
+                        minlength: 5,
+                        maxlength: 20
+                    },
+                    password_confirmation: {
+                        required: true,
+                        equalTo: "#password"
+                    }
                 },
-                submitHandler: function (form) { // ketika valid, maka bagian yg akan dijalankan
+                submitHandler: function (form) {
                     $.ajax({
                         url: form.action,
                         type: form.method,
                         data: $(form).serialize(),
                         success: function (response) {
-                            if (response.status) { // jika sukses
+                            if (response.status) {
                                 Swal.fire({
                                     icon: 'success',
                                     title: 'Berhasil',
@@ -111,7 +150,7 @@ ack">
                                 }).then(function () {
                                     window.location = response.redirect;
                                 });
-                            } else { // jika error
+                            } else {
                                 $('.error-text').text('');
                                 $.each(response.msgField, function (prefix, val) {
                                     $('#error-' + prefix).text(val[0]);
